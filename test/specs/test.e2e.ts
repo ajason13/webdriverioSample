8e0c09e621561733234f21a806dc0fcffa8b8e6e
Jason Alvarez <a.jason13@gmail.com>
2024-03-08 17:51:50 -0800

    added login error scenarios

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -2,7 +2,7 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-describe('My Login application', () => {
+describe('My Login page', () => {
   it('should login with valid credentials', async () => {
     await LoginPage.open()
 
@@ -10,6 +10,26 @@ describe('My Login application', () => {
     await expect(SecurePage.flashAlert).toBeExisting()
     await expect(SecurePage.flashAlert).toHaveText(
       expect.stringContaining('You logged into a secure area!')
+    )
+  })
+
+  it('should error with invalid credentials', async () => {
+    await LoginPage.open()
+
+    await LoginPage.login('incorrect', 'login')
+    await expect(LoginPage.flashAlert).toBeExisting()
+    await expect(LoginPage.flashAlert).toHaveText(
+      expect.stringContaining('Your username is invalid!')
+    )
+  })
+
+  it('should error with no credentials', async () => {
+    await LoginPage.open()
+
+    await LoginPage.btnSubmit.click()
+    await expect(LoginPage.flashAlert).toBeExisting()
+    await expect(LoginPage.flashAlert).toHaveText(
+      expect.stringContaining('Your username is invalid!')
     )
   })
 })
